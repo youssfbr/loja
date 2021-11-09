29a726836498409bb2b258d3a26d3777e6c00d53
Alisson Youssf <youssfbr@gmail.com>
2021-11-09 12:04:22 -0300

    Backend Java: Project created

---
+++ backend-spring/src/test/java/com/github/youssfbr/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
