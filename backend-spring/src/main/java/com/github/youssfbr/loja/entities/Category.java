9aa3caa4cb8cf2dda6e33480d23b0422958573a6
Alisson Youssf <youssfbr@gmail.com>
2021-11-09 19:01:04 -0300

    Backend Java: Category Auditing data

--- backend-spring/src/main/java/com/github/youssfbr/loja/entities/Category.java
+++ backend-spring/src/main/java/com/github/youssfbr/loja/entities/Category.java
@@ -1,22 +1,42 @@
 package com.github.youssfbr.loja.entities;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.Instant;
 
 @Entity
 @Table(name = "tb_category")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Getter@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Category {
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    private void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        updatedAt = Instant.now();
+    }
 
 }
